164844aa36d3e289963b33ae30e54a8f2f046128
Jamie.vg | Easyzly <117265805+Easyzly@users.noreply.github.com>
2025-02-10 14:44:01 +0100

    +log class & add form to submit a new log

--- main.js
+++ main.js
@@ -5,7 +5,28 @@ const descriptionInput = document.getElementById('description-input');
 const costInput = document.getElementById('cost-input');
 const submitButton = document.getElementById('submit-button');
 
+class log {
+    constructor(name, description, cost) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+    }
+}
+
 toggleButton.addEventListener('click', () => {
    createMenu.classList.toggle('hidden');
    console.log('clicked');
+});
+
+submitButton.addEventListener('click', () => {
+    const name = nameInput.value;
+    const description = descriptionInput.value;
+    const cost = costInput.value;
+    const newLog = new log(name, description, cost);
+    console.log(newLog);
+    nameInput.value = '';
+    descriptionInput.value = '';
+    costInput.value = '';
+    createMenu.classList.toggle('hidden');
+    console.log('submitted');
 });
